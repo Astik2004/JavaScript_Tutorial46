80679dd9b8f40da411a2b6e3fec48565e0ea9691
Astik Yadav <114437794+Astik2004@users.noreply.github.com>
2024-07-16 06:20:07 +0000

    js

--- basicJS/function.js
+++ basicJS/function.js
@@ -5,3 +5,13 @@ function sayMyname(){
     console.log("Astik Yadav");
 }
 sayMyname();
+//function with object
+const user={
+    username:"Hitesh",
+    price:199
+}
+
+function handleObj(anyobj){
+    console.log(`Username is ${anyobj.username} and price is ${anyobj.price}`);
+}
+handleObj(user);
